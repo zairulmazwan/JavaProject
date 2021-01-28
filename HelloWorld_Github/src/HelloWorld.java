5027cbb258228e5cc66cfb522a366ac617bfefc9
Zairul Mazwan <zairulmazwan@gmail.com>
2021-01-28 18:19:58 +0000

    Version 1.01 - Added a new message.

---
+++ HelloWorld_Github/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	
+	public static void main (String [] args)
+	{
+		System.out.println("Hello Github!");
+		System.out.println("This is my first Github commit and push");
+		
+	}
+
+}
